ebdb9325843b9c78acdba32ed929bdec918adeaf
vasilache <mihai_vasilache@yahoo.com>
2017-05-09 22:47:03 +0300

    improvet bat

--- reload.bat
+++ reload.bat
@@ -3,5 +3,17 @@ cd %folder%
 for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
 cd ..\../proiecte/coderdojo.git/coderdojo-projects/ctf/
 call mvn clean install
+if %ERRORLEVEL%  NEQ 0 ( GOTO ERROR )
 cd ..\..\..\..\spigot
 java -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=4020 -jar spigot-1.11.2.jar
+
+goto EOF
+
+:ERROR
+cd ..\..\..\..\spigot
+echo *****************
+echo COMPILATION ERROR
+echo *****************
+
+
+:EOF
